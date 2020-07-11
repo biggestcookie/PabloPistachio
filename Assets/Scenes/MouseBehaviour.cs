ec9d48377869e2ed2be341384d1ce265904102b5
TJ Hasan <tjhasan159@gmail.com>
2020-07-10 22:26:56 -0400

    Created new behavior for the pen. Will now randomly change width

---
+++ Assets/Scenes/MouseBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class MouseBehaviour : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(double X, double Y);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int rand = Random.Range(0, 1000);
+        if (rand == 1)
+        {//change is done randomly
+            StartCoroutine(WaitAndDoSomething());
+        }
+    }
+
+    IEnumerator WaitAndDoSomething()
+    {
+        FreeDraw.Drawable.Pen_Width = 10;
+        yield return new WaitForSeconds(1f);//change duration using this
+        FreeDraw.Drawable.Pen_Width = 3;
+    }
+}
