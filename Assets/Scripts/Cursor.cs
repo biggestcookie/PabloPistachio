8dc83fa86349f4110d5e48935437706ed041239d
tsyoo <tsyookr@gmail.com>
2020-07-11 18:28:47 -0400

    Remove size selector

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -6,13 +6,10 @@ using System.Runtime.InteropServices;
 public class Cursor : MonoBehaviour
 {
     public float speed;
-    public float accel;
-    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         Screen.lockCursor = true;
-        rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -25,18 +22,7 @@ public class Cursor : MonoBehaviour
     {
         float x = Input.GetAxis("Mouse X") / 10;
         float y = Input.GetAxis("Mouse Y") / 10;
-        float x_dir = x / Mathf.Abs(x);
-        float y_dir = y / Mathf.Abs(y);
-        // Debug.Log("Mouse X: " + x + ", Mouse Y: " + y);
         float step = speed * Time.deltaTime;
-        //x = Mathf.MoveTowards(x, speed * Input.GetAxisRaw("Mouse X"), accel * Time.deltaTime);
-        //y = Mathf.MoveTowards(y, speed * Input.GetAxisRaw("Mouse Y"), accel * Time.deltaTime);
         return Vector3.MoveTowards(this.gameObject.transform.position, this.gameObject.transform.position + new Vector3(x, y, 0), step);
-        //this.gameObject.transform.move += new Vector3(Input.GetAxis("Mouse X") * .25f, Input.GetAxis("Mouse Y") * .25f, 0);
-    }
-
-    void OnMouseDown()
-    {
-
     }
 }
