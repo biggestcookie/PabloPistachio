a88920172fe832951aa9e72f9f480519e9c3c0da
tsyoo <tsyookr@gmail.com>
2020-07-12 03:00:21 -0400

    Added park background scenes and placed art assets in art folder

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -15,7 +15,6 @@ public class Cursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Screen.width);
         Screen.lockCursor = true;
     }
 
